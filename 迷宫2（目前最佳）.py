bf296e685088e7b6589a781f32614102ad23b266
ucyuunaka <cuiylin@mail2.sysu.edu.cn>
2025-10-30 17:18:21 +0800

    最佳的迷宫

---
+++ 迷宫2（目前最佳）.py
@@ -0,0 +1,33 @@
+while True:
+	if get_entity_type() != Entities.Bush:
+		if can_harvest():
+			harvest()
+		plant(Entities.Bush)
+	
+	substance = get_world_size() * 2**(num_unlocked(Unlocks.Mazes) - 1)
+	
+	use_item(Items.Weird_Substance, substance)
+	
+	directions = [North, East, South, West]
+	direction_index = 0
+	
+	while get_entity_type() != Entities.Treasure:
+		right_index = (direction_index + 1) % 4
+		forward_index = direction_index
+		left_index = (direction_index - 1) % 4
+		back_index = (direction_index + 2) % 4
+		
+		if can_move(directions[right_index]):
+			move(directions[right_index])
+			direction_index = right_index
+		elif can_move(directions[forward_index]):
+			move(directions[forward_index])
+		elif can_move(directions[left_index]):
+			move(directions[left_index])
+			direction_index = left_index
+		else:
+			move(directions[back_index])
+			direction_index = back_index
+	
+	harvest()
+	quick_print("黄金总数:", num_items(Items.Gold))
